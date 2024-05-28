32eb2bcd1b8f6c17f79bf8b6a13688cc7b7c6190
jasurmavlonov02 <jasurmavlonov24@gmail.com>
2024-05-28 15:40:02 +0500

    first commit

---
+++ app/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from app.models import Product, Comment
+
+
+class ProductForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    description = forms.CharField(widget=forms.Textarea)
+    price = forms.FloatField()
+    image = forms.ImageField()
+    rating = forms.ChoiceField(choices=Product.RatingChoices.choices)
+    discount = forms.IntegerField()
+
+
+class ProductModelForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        # fields = ['name', 'description', 'price', 'image', 'rating', 'discount']
+        exclude = ()
+
+
+class CommentModelForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['full_name', 'email', 'message']
